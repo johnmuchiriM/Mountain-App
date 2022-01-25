ac5e811020ec3fd69e9da7c0d545de90b0ba7f5d
johnmuchiriM <jeanfrancaiseharicot@gmail.com>
2022-01-25 10:02:04 +0300

    update:managed to draw circle

---
+++ lib/pages/navpages/home_page.dart
@@ -0,0 +1,127 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 70, left: 20),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.menu,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                  const Spacer(),
+                  Container(
+                    margin: const EdgeInsets.only(right: 20),
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.withOpacity(0.5)),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(
+              text: 'Discover',
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          //TabBar
+          Container(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                  labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  unselectedLabelColor: Colors.grey,
+                  isScrollable: true,
+                  indicator:
+                      CircleTabIndicator(color: AppColors.mainColor, radius: 4),
+                  tabs: const [
+                    Tab(text: 'Places'),
+                    Tab(text: 'Inspiration'),
+                    Tab(text: 'Emotions')
+                  ]),
+            ),
+          ),
+          Container(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(
+              controller: _tabController,
+              children: const [
+                Text("Hi"),
+                Text("There"),
+                Text("Bye"),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+
+  CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    // TODO: implement createBoxPainter
+    return _CirclePainter(color: color, radius: radius);
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    // TODO: implement paint
+
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    final Offset circleOffset = Offset(
+        configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius);
+
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
+  }
+}
---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/pages/navpages/bar_item_page.dart';
+import 'package:flutter_cubit/pages/navpages/home_page.dart';
+import 'package:flutter_cubit/pages/navpages/my_page.dart';
+import 'package:flutter_cubit/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    const HomePage(),
+    const BarItemPage(),
+    const SearchPage(),
+    const MyPage(),
+  ];
+
+  int currentIndexPage = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndexPage = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndexPage],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndexPage,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(title: Text('Home'), icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(
+              title: Text('Bar'), icon: Icon(Icons.bar_chart_outlined)),
+          BottomNavigationBarItem(
+              title: Text('Search'), icon: Icon(Icons.search)),
+          BottomNavigationBarItem(title: Text('My'), icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
